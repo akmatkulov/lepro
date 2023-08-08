89ae910f7c406c67f7120a230a441340263c7af8
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2023-08-08 15:49:38 +0600

    Add database

--- app.rb
+++ app.rb
@@ -5,7 +5,24 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def init_db
+  @db=SQLite3::Database.new 'lepro.db'
+  @db.results_as_hash = true
+end
+
+before do
+  init_db
+end
 
 get '/' do
-  "Hello"
+  erb "Hello"
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/new' do
+  @content = params['text']
+  erb "You typed #{@content}"
 end
