d8391e181d10861dfb87af2b5111869f45196a81
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2023-08-08 15:13:33 +0600

    initial app

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'sinatra', '~> 3.0', '>= 3.0.6'
+gem 'sinatra-reloader', '~> 1.0'
---
+++ app.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+
+get '/' do
+  "Hello"
+end
