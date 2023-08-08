37174466d54ee2633de8e69edc8e1eded63f608b
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2023-08-08 16:37:27 +0600

    Add post link

--- app.rb
+++ app.rb
@@ -7,22 +7,39 @@ require 'sqlite3'
 
 def init_db
   @db=SQLite3::Database.new 'lepro.db'
-  @db.results_as_hash = true
 end
 
 before do
   init_db
 end
 
-get '/' do
-  erb "Hello"
+configure do
+    
+    @db.execute 'CREATE TABLE IF NOT EXISTS
+    "Posts"
+      (
+        "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+        "created_at" DATE,
+        "content" TEXT
+      )'
 end
 
+get '/' do
+  @results = @db.execute 'select * from Posts order by id desc'
+  erb :index
+end  
 get '/new' do
   erb :new
 end
 
 post '/new' do
   @content = params['text']
-  erb "You typed #{@content}"
+  if @content.length <= 0
+    @error = 'Type post text'
+    return erb :new
+  end
+
+  @db.execute 'insert into Posts (content, created_at) values (?,datetime())',[@content]
+
+  redirect to '/'
 end
